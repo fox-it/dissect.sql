bde8e2d712cf552a7c4bd107a5ec62c7557f3cd2
Computer Network Investigation <121175071+JSCU-CNI@users.noreply.github.com>
2024-03-07 17:44:12 +0100

    Improve text type handling for sqlite3 values (#19)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import os
+from typing import BinaryIO
 
 import pytest
 
 
-def open_data(name):
+def open_data(name: str) -> BinaryIO:
     return open(os.path.join(os.path.dirname(__file__), name), "rb")
 
 
 @pytest.fixture
-def sqlite_db():
+def sqlite_db() -> BinaryIO:
     return open_data("data/test.sqlite")
